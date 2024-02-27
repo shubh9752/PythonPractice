8ce2782978383a95d5aacc84a8ab5206e210171c
shubh9752 <shubhamkumar3.sk@gmail.com>
2024-02-27 14:14:51 +0530

    first commit

---
+++ list/makingList.py
@@ -0,0 +1,42 @@
+# l=[10,20,30,40,50]
+# # searching and adding
+# print(l)
+# print(l[3])
+# print(l[-1])
+# print(l.append(60))
+# print(l)
+# print(l.insert(4,60))
+# print(l)
+# print(20 in l)
+# print(l.index(20))
+# print(l.count(60))
+# print(l.index(50,4,7))
+# # remval
+# print(l.remove(60))#it removes only first element that it searches but if there's no elemnt that tell for remove it will going to give you an error
+
+# print(l)
+# print(l.pop())#arr.pop removes last index element of arr
+# print(l)
+# print(l.pop(1))#arr.pop(index that you want to remove)
+# print(l)
+# del l[1]#del arr[index of element]
+# print(l)
+# del l[0:2]#del arr[start index:number of elements]
+# print(l)
+
+
+# some genereal pupose functions
+l1=[10,20,30,"apple",True,40,40.5]
+# max(l1)it will not supported if your arr have strings in it
+l2=[10,20,30,40,50,60,60.5]
+print(max(l2))
+print(min(l2))
+print(sum(l2))
+print(l2.reverse())
+print(l2)
+print(l1.reverse())
+print(l1)
+l2.sort()
+print(l2)
+l1.sort()
+print(l1)
